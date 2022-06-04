241c7752d36a80c5ee6f70f460505be244f9553f
Med. Amine Aljane <73314263+MedAljane@users.noreply.github.com>
2022-06-04 11:38:49 +0100

    Added a module and modified voice recognition
    - Added a script to try TripAware
    - Added class for the speaker and recognizer
    - Added French and English voice outputs to the TripAware module
    - Working on adding criteria choosing for the best results
    - Working on adding french voice output for the results

---
+++ tryTripAware.py
@@ -0,0 +1,39 @@
+from voiceSetup import Speaker
+from Tripaware.tripaware import getTripAware
+
+sp = Speaker("en")
+
+sp.talk("Please choose language! (English or French)")
+sp.setLanguage("fr")
+sp.talk("Veuillez choisir la langue! (Anglais ou français)")
+
+language = sp.take_command()
+
+validLanguage = False
+while not validLanguage:
+    if language.lower() in ["english","anglais","french","francais","français"]:
+        if language.lower() in ["french","francais","français"]:
+            sp.setLanguage('fr')
+            language = 'fr'
+        elif language.lower() in ['english', 'anglais']:
+            sp.setLanguage('en')
+            language = 'en'
+        validLanguage = True
+    else:
+        sp.setLanguage("en")
+        sp.talk("Language not supported! Please choose language between: English or French)")
+        sp.setLanguage("fr")
+        sp.talk("Langue non prise en charge ! Veuillez choisir la langue entre : anglais ou français)")
+        
+        language = sp.take_command()
+
+
+if language == 'en':
+    sp.talk("Insert the mean of transport please!")
+elif language == 'fr':
+    sp.talk("Insérez le moyen de transport s'il vous plaît!")
+
+
+transportMethod = sp.take_command()
+
+getTripAware(transportMethod, sp)
---
+++ voiceSetup.py
@@ -0,0 +1,74 @@
+import speech_recognition as sr                 # For voice recognition
+import pyttsx3                                  # For text to speech conversion
+
+class Speaker:
+    global lang
+    global engine
+
+
+    def __init__(self, language):
+        self.listener = sr.Recognizer()
+        self.engine = pyttsx3.init()
+        if language == "fr":
+            self.engine.setProperty('voice', self.engine.getProperty('voices')[2].id)
+        elif language == "en":
+            self.engine.setProperty('voice', self.engine.getProperty('voices')[0].id)
+        self.engine.setProperty('rate', 200)
+        self.engine.setProperty('volume', 1)
+        self.lang = language
+    
+    def setLanguage(self, language):
+        if language == "fr":
+            self.engine.setProperty('voice', self.engine.getProperty('voices')[2].id)
+            self.lang = language
+        elif language == "en":
+            self.engine.setProperty('voice', self.engine.getProperty('voices')[0].id)
+            self.lang = language
+
+
+    def talk(self, text):
+        print(text)
+        self.engine.say(text)
+        self.engine.runAndWait() 
+
+
+    def take_command(self):
+        if self.lang == 'fr':
+            recognized = False
+            while not recognized:
+                with sr.Microphone() as source:
+                    self.listener.adjust_for_ambient_noise(source, duration=1)
+                    self.talk("J'ecoute...")
+                    self.listener.pause_threshold = 1
+                    audio = self.listener.listen(source)
+            
+                try:
+                    self.talk("En train de reconnaître...") 
+                    command = self.listener.recognize_google(audio, language ='fr')
+                    print(f"Vous --->: {command}\n")
+                    recognized = True
+            
+                except Exception as e:
+                    self.talk("Impossible de reconnaître votre voix, veuillez réessayer...")
+            
+            return command
+
+        elif self.lang == 'en':
+            recognized = False
+            while not recognized:
+                with sr.Microphone() as source:
+                    self.listener.adjust_for_ambient_noise(source, duration=1)
+                    self.talk("Listening...")
+                    self.listener.pause_threshold = 1
+                    audio = self.listener.listen(source)
+            
+                try:
+                    self.talk("Recognizing...") 
+                    command = self.listener.recognize_google(audio, language ='en')
+                    print(f"you --->: {command}\n")
+                    recognized = True
+            
+                except Exception as e:
+                    self.talk("Unable to Recognize your voice, try Again please...")
+            
+            return command
